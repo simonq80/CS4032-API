fc38fe13ea6eeaec32ea1c38b3a3c49b725b3c32
simonq80 <simonq80@gmail.com>
2017-02-05 17:14:02 +0000

    file server api added

---
+++ src/FileAPI.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE DeriveAnyClass       #-}
+{-# LANGUAGE DeriveGeneric        #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE StandaloneDeriving   #-}
+{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+
+module FileAPI where -- (getSecurityUsers, getSecurityUser, getPackages, Package(..))  where
+
+import           Control.Monad       (mzero)
+import           Data.Aeson
+import           Data.Proxy
+import           Data.Text
+import           GHC.Generics
+import           Network.HTTP.Client (defaultManagerSettings, newManager)
+import           Servant.API
+import           Servant.Client
+import           SecurityAPI
+import           ClientProxyAPI
+
+
+
+
+type FileAPI = "readfile" :> ReqBody '[JSON] (FileDetails, ServerDetails):> Post '[JSON] (Maybe FileDetails)
+             :<|> "writefile" :> ReqBody '[JSON] (FileDetails, ServerDetails) :> Post '[JSON] Bool
+
+
+fileAPI :: Proxy FileAPI
+fileAPI = Proxy
+
+readFile :: (FileDetails, ServerDetails)-> ClientM (Maybe FileDetails)
+writeFile :: (FileDetails, ServerDetails) -> ClientM Bool
+
+readFile :<|> writeFile = client fileAPI
